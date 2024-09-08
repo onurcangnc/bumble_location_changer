42ffb8a7057105ec76d3cdc2725cf256139697d6
Onurcan Genç <onurcan.genc@ug.bilkent.edu.tr>
2024-09-08 05:32:22 +0300

    updated.

--- location_changer.js
+++ location_changer.js
@@ -2,21 +2,22 @@
 // @name         Bumble Auto Location Changer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Chrome based tampermonkey extension for location change.
+// @description  Chrome based Tampermonkey extension for location change.
 // @author       Onurcan
 // @match        https://bumble.com/*
 // @match        https://*.bumble.com/*
+// @match        https://*.gps-coordinates.net/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Balıkesir Koordinatları
-    const latitude = 39.6357;
-    const longitude = 27.8927;
+    // New Coordinates (near Balikesir)
+    const latitude = 39.632289;  // Latitude (Enlem)
+    const longitude = 27.896004; // Longitude (Boylam)
 
-    // Tarayıcının Geolocation API'sini geçersiz kıl
+    // Override the browser's Geolocation API to use the custom coordinates
     navigator.geolocation.getCurrentPosition = function(success, error) {
         success({
             coords: {
